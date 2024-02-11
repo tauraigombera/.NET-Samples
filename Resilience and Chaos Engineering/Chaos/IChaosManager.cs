42cd5bceba0de398c9d5de468071f994c6b8cb13
TauG <tauraigombera@gmail.com>
2024-02-11 19:15:30 +0200

    added IChaosManager

---
+++ Resilience and Chaos Engineering/Chaos/IChaosManager.cs
@@ -0,0 +1,10 @@
+﻿using Polly;
+
+namespace Chaos;
+
+public interface IChaosManager
+{
+    ValueTask<bool> IsChaosEnabledAsync(ResilienceContext context);
+
+    ValueTask<double> GetInjectionRateAsync(ResilienceContext context);
+}
