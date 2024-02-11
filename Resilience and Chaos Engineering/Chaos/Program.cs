e389bd3da483718e5fecf947211b1ebe66c51d3a
TauG <tauraigombera@gmail.com>
2024-02-11 17:46:16 +0200

    modified the Program.cs

--- Resilience and Chaos Engineering/Chaos/Program.cs
+++ Resilience and Chaos Engineering/Chaos/Program.cs
@@ -1,6 +1,10 @@
+using Chaos;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+var httpClientBuilder = builder.Services.AddHttpClient<TodosClient>(client => client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com"));
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", (TodosClient client, CancellationToken cancellationToken) => client.GetTodosAsync(cancellationToken));
 app.Run();
