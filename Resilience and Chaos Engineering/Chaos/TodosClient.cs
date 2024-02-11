02edea6419326daecfba4dc5afe9af59de04f16e
TauG <tauraigombera@gmail.com>
2024-02-11 17:36:03 +0200

    added TodosClient

---
+++ Resilience and Chaos Engineering/Chaos/TodosClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Chaos;
+
+public class TodosClient(HttpClient client)
+{
+    public async Task<IEnumerable<TodoModel>> GetTodosAsync(CancellationToken cancellationToken)
+    {
+        return await client.GetFromJsonAsync<IEnumerable<TodoModel>>("/todos", cancellationToken) ?? [];
+    }
+}
